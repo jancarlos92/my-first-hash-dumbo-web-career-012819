f4df3e6b2c01a8efe3b31f698530e1d588adc2c9
JanCarlos Acevedo <jancarlos92@me.com>
2019-01-14 16:18:20 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -10,10 +10,10 @@ def shipping_manifest
 
 end
 
-def retrieval(item)
+def retrieval("key")
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3} 
   
-  shipping_manifest["item"]
+  shipping_manifest["key"]
  
  end
 
