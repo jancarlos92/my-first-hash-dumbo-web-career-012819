cd36e6dc192ebf742a033690700b393b9888fbd9
JanCarlos Acevedo <jancarlos92@me.com>
2019-01-14 15:56:06 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,7 +13,7 @@ end
 def retrieval(num)
     num = "num"
     
-    shipping_manifest = {"whale bone corsets" => 5, 
+    shipping_manifest = { "whale bone corsets" => 5, 
     "porcelain vases" => 2, 
      "oil paintings" => 3}
    
